4d7ee5975d4b426adc1bd1fa2f6f32ac5d087e9e
Kais Dukes <dukeskais79@gmail.com>
2022-06-18 15:33:38 +0100

    Mirror ends.

--- src/main/java/codingbat/string3/MirrorEnds.java
+++ src/main/java/codingbat/string3/MirrorEnds.java
@@ -4,10 +4,8 @@ public class MirrorEnds {
 
     public static String mirrorEnds(final String string) {
         for (var m = 1; m <= string.length(); m++) {
-            for (var i = 0; i < m; i++) {
-                if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
-                    return string.substring(0, m - 1);
-                }
+            if (string.charAt(m - 1) != string.charAt(string.length() - m)) {
+                return string.substring(0, m - 1);
             }
         }
         return string;
