b5b5a4ce2926721c0d68a2383b1f85ce9abbebf2
Kais Dukes <dukeskais79@gmail.com>
2022-06-18 13:02:24 +0100

    Code clean.

--- src/main/java/codingbat/string2/PrefixAgain.java
+++ src/main/java/codingbat/string2/PrefixAgain.java
@@ -4,8 +4,7 @@ public class PrefixAgain {
 
     public static boolean prefixAgain(final String str, final int n) {
         final var prefix = str.substring(0, n);
-        final var p = prefix.length();
-        for (var i = p; i < str.length() - p + 1; i++) {
+        for (var i = n; i < str.length() - n + 1; i++) {
             if (str.startsWith(prefix, i)) return true;
         }
         return false;
