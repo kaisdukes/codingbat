f5d4df8516eedd373aab955706a626beb0487e39
Kais Dukes <dukeskais79@gmail.com>
2022-06-20 13:14:00 +0100

    Split odd 10.

--- src/main/java/codingbat/recursion2/SplitOdd10.java
+++ src/main/java/codingbat/recursion2/SplitOdd10.java
@@ -1,13 +1,16 @@
 package codingbat.recursion2;
 
-public class SplitArray {
+public class SplitOdd10 {
 
-    public static boolean splitArray(final int[] nums) {
+    public static boolean splitOdd10(final int[] nums) {
         return sumGroups(0, nums, 0, 0);
     }
 
     private static boolean sumGroups(final int start, final int[] nums, final int sum1, final int sum2) {
-        if (start >= nums.length) return sum1 == sum2;
+        if (start >= nums.length) {
+            return sum1 % 10 == 0 && sum2 % 2 == 1
+                    || (sum2 % 10 == 0 && sum1 % 2 == 1);
+        }
         return sumGroups(start + 1, nums, sum1 + nums[start], sum2)
                 || sumGroups(start + 1, nums, sum1, sum2 + nums[start]);
     }
